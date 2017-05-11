fa247d2b190f35511e1265a450413f240ab3c396
xamgore <xamgore@ya.ru>
2017-05-11 22:15:48 +0300

    Polyfill es6/es7 features with babel

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
+rqeuire('babel-polyfill')
 require('babel-register')({
-    plugins: [
-        'transform-es2015-modules-commonjs',
-        'transform-object-rest-spread',
-        // 'syntax-async-functions',
-        // 'syntax-async-generators',
-    ]
+  presets: ['env'],
+  plugins: ['transform-object-rest-spread']
 })
-
-// require('babel-polyfill')
 
 
 var fs = require('fs')
 
 if (!fs.existsSync('.env'))
   fs.writeFileSync('.env', fs.readFileSync('.env.example'))
+
 
 require('dotenv').config()
 require('./src/app')
