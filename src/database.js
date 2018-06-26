c01868040a5f6479b2ab7a0288e557ce943b60f2
xamgore <xamgore@ya.ru>
2018-06-26 21:20:37 +0300

    Save data in YAML

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 import lowdb from 'lowdb'
+import { dump as serialize, load as deserialize } from 'js-yaml'
 import FileAsync from 'lowdb/adapters/FileAsync'
 
 const db =
-  lowdb(new FileAsync('db.json'))
-    .then(db => { db.defaults({ posts: [] }).write(); return db })
+  lowdb(new FileAsync('db.yaml', { defaultValue: { posts: [] }, serialize, deserialize }))
 
 export default db
