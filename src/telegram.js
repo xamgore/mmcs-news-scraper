69dce37b1a35759a351cdd1a41066f80a6cac9ce
xamgore <xamgore@ya.ru>
2019-05-02 12:10:38 +0300

    Add https proxy

--- src/telegram.js
+++ src/telegram.js
@@ -1,18 +1,26 @@
 import Socks5HttpsAgent from 'socks5-https-client/lib/Agent'
 import TelegramBot from 'node-telegram-bot-api'
 
+
+const socksProxy = {
+  agentClass: Socks5HttpsAgent,
+  agentOptions: {
+    // http://spys.one/en/socks-proxy-list/
+    socksHost: 'ns3005469.ip-151-80-100.eu',
+    socksPort: 54005,
+    // socksUsername: '',
+    // socksPassword: '',
+  },
+}
+
+const httpsProxy = {
+  // http://spys.one/en/https-ssl-proxy/
+  proxy: "https://128.199.172.233:8080",
+}
+
 const bot = new TelegramBot(process.env.MMCS_NEWS_TELEGRAM_TOKEN, {
   polling: false,
-  request: {
-    agentClass: Socks5HttpsAgent,
-    agentOptions: {
-      // http://spys.one/en/socks-proxy-list/
-      socksHost: 'ns3005469.ip-151-80-100.eu',
-      socksPort: 54005,
-      // socksUsername: '',
-      // socksPassword: '',
-    },
-  },
+  request: httpsProxy,
 })
 
 // change polling to true, so you will get chat id from bot
